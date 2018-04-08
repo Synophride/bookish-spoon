ac4f06096b0c2441f13424577146ff9fd37b0154
John Doe <synophride@EL.MONO>
2018-04-08 16:59:39 +0200

    ajout exemplles

--- top.ml
+++ top.ml
@@ -1,11 +1,6 @@
 open Format;;
-open Lexing;;
-open Lexer;;
-open Parser;;
-open Ast;;
 open Interpretation;;
-open Typing;;
-
+open Types;;
 let file_test = ["ex1.miniml" ; "ex2.miniml"; "ex3.miniml"]
 ;;
 
@@ -15,13 +10,11 @@ let file_descriptor_test = List.map (open_in) file_test
 let evt = ref 0;;
 (* ou p_def list * types list * values list *)
 (* Semble plus simple à implémenter mais faire attention aux conneries *)
-let () =
-  Printf.printf "\t miniML version 0.1\n\n"
+
+Printf.printf "\t miniML version 0.1\n\n"
     
 (** Create a lexer buffer which reads from the given string **)
 let rec top () = 
-  while  1 = 1 do
-    try
       List.iter
 	(fun e ->
 	  let lb = Lexing.from_channel e in
@@ -30,9 +23,6 @@ let rec top () =
 	  List.iter (fun x -> Printf.printf "%s\n" (str_value (x))) plets_interpreted
 	)
 	(file_descriptor_test )
-    with
-      Lexical_error s -> ()
-    | Parsing.Parse_error -> ()
-  done;  
 ;;
 
+top ();;
